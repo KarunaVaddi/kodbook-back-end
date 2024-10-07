a6f4f15508d61dafd2a52e481e5d9023c16c2772
Karuna Vaddi <160813672+KarunaVaddi@users.noreply.github.com>
2024-10-07 12:05:42 +0530

    Update UserRepository.java

--- com.react.kodbooks/src/main/java/com/kodbooks/repositories/UserRepository.java
+++ com.react.kodbooks/src/main/java/com/kodbooks/repositories/UserRepository.java
@@ -2,8 +2,6 @@ package com.kodbooks.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.kodbooks.entities.User;
-
 public interface UserRepository 
 		extends JpaRepository<User, Long>{
 
